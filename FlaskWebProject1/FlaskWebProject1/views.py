a1aafa8a2b977ca030d2d9fdf54fb3a792048528
tsou0320 <tsou0320@gmail.com>
2019-12-31 13:34:51 +0800

    add  search box

--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -34,43 +34,30 @@ def contact():
 @app.route('/search/<string:name>')
 def about(name= None):
     """Renders the about page."""
-    #cities = [tpe, nyc, ldn]
-
-    coursemap = pd.read_excel('/Users/helen/Desktop/DataBase/FlaskWebProject1/FlaskWebProject1/coursemap.xlsx')
-    #print(coursemap.shape)
-    coursemap_list = {}
-    nrows = coursemap.shape[0]
-    for i in range(nrows):
-        ser = coursemap.loc[i, :]
-        
-        #print(ser.index,ser.values)
-        row_dict = {}
-
-        for idx, val in zip(ser.index, ser.values):
-            idx = str(idx)
-            if type(val) is str:
-                row_dict[idx] = val
-            elif type(val) is np.int64:
-                row_dict[idx] = int(val)
-            elif type(val) is np.float64:
-                row_dict[idx] = float(val)
-            #print(row_dict)
-        #coursemap_list.append(row_dict)
-        coursemap_list[str(i)] = row_dict
-    #print(coursemap_list)
-    coursemap_user = jsonify(coursemap_list)
-
-    #user = {'name':'Michael', 'age':18, 'scores':[{'course': 'Math', 'score': 76}]}
-    users = mongo.db.users.find()
-    # mongo.db.users.remove({})
-    if users.count() is 0:
-        for k, v in coursemap_list.items():
-            mongo.db.users.insert(v)
+    # coursemap = pd.read_excel('/Users/helen/Desktop/DataBase/FlaskWebProject1/FlaskWebProject1/json_zip_1.xlsx')
+    # coursemap_list = {}
+    # nrows = coursemap.shape[0]
+    # for i in range(nrows):
+    #     ser = coursemap.loc[i, :]
+    #     row_dict = {}
+    #     for idx, val in zip(ser.index, ser.values):
+    #         idx = str(idx)
+    #         if type(val) is str:
+    #             row_dict[idx] = val
+    #         elif type(val) is np.int64:
+    #             row_dict[idx] = int(val)
+    #         elif type(val) is np.float64:
+    #             row_dict[idx] = float(val)
+    #     coursemap_list[str(i)] = row_dict
+    # coursemap_user = jsonify(coursemap_list)
+    users1 = mongo.db.users1.find()
+    users2 = mongo.db.users2.find()
+    # if users2.count() is 0:
+    #     for k, v in coursemap_list.items():
+    #         mongo.db.users2.insert(v)
     if name is None:
-        users = mongo.db.users.find()
-        #print(users)
-        if users is not None:
-            return render_template('users.html',  users=users)
+        if users2 is not None:
+            return render_template('users.html',  users=users2)
         else:
             return 'No user found!'
          
